0e8722d91a051ccd485fb03d6f2668aced3f5ecb
Marcin Wincek <mwincek005@gmail.com>
2015-10-12 20:58:28 +0200

    App skeleton

---
+++ client/components/footer/footer.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('dotCinemaApp')
+  .directive('footer', function () {
+    return {
+      templateUrl: 'components/footer/footer.html',
+      restrict: 'E',
+      link: function (scope, element) {
+        element.addClass('footer');
+      }
+    };
+  });
