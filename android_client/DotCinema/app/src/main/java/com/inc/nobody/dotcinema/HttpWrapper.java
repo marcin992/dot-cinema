72015746ad48b06db5107582a80609b70362fff0
MaciejSzaflik <maciej.szaflik@gmail.com>
2015-12-08 23:56:49 +0100

    http wrapper refractor

--- android_client/DotCinema/app/src/main/java/com/inc/nobody/dotcinema/HttpWrapper.java
+++ android_client/DotCinema/app/src/main/java/com/inc/nobody/dotcinema/HttpWrapper.java
@@ -1,21 +1,39 @@
 package com.inc.nobody.dotcinema;
 
+import android.util.Pair;
+
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.AbstractHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by maciej on 06/12/15.
  */
 public class HttpWrapper {
+    final String mainUrl = "http://91.196.50.18:8088";
+    final String loginUrl = "/auth/local";
+    final String registerUrl = "/api/users";
+
+    public String getLoginUrl()
+    {
+        return mainUrl + loginUrl;
+    }
+    public String getRegisterUrl()
+    {
+        return mainUrl + registerUrl;
+    }
 
     private static HttpWrapper instance;
 
@@ -31,72 +49,83 @@ public class HttpWrapper {
     }
     public String MakePostLogin(String email, String password) {
 
-        HttpClient httpClient = new DefaultHttpClient(); //Deprecated
+        Pair<String,Object>[] parameters = new Pair[]{
+                new Pair("email",email),
+                new Pair("password",password),
+        };
+        String response = MakePost(getLoginUrl(),parameters);
+        System.out.println(response);
+        return response;
+    }
+
+    public String MakePostRegistration(String email, String password,String nick)
+    {
+        Pair<String,Object>[] parameters = new Pair[]{
+                new Pair("email",email),
+                new Pair("password",password),
+                new Pair("nick",nick)
+        };
+        String response = MakePost(getRegisterUrl(),parameters);
+        System.out.println(response);
+        return response;
+    }
+
+    public String MakePost(String url,Pair<String,Object> ... params) {
+
+        HttpClient httpClient = getHttpClient(); //Deprecated
 
         try {
-            HttpPost request = new HttpPost("http://91.196.50.18:8088/auth/local");
-
-            StringBuilder json = new StringBuilder();
-            json.append("{\"email\":\"");
-            json.append(email);
-            json.append("\",\"password\":\"");
-            json.append(password);
-            json.append("\"}");
-
-            StringEntity jsonParams =new StringEntity(json.toString());
+            HttpPost request = new HttpPost(url);
+            StringEntity jsonParams =new StringEntity(createJsonString(params).toString());
             request.addHeader("Content-Type", "application/json");
             request.setEntity(jsonParams);
             HttpResponse response = httpClient.execute(request);
 
             // handle response here...
             JSONObject message = GetJSON(PrintHttpResponse(response));
-            return message.getString("message");
-        }catch (Exception ex) {
-            return ex.toString();
-
-        } finally {
+            return  message.toString();
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return "UnsupportedEncodingException";
+        }
+        catch (ClientProtocolException e) {
+            e.printStackTrace();
+            return "ClientProtocolException";
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return "IOException";
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+            return "JSONException";
+        }
+        finally {
             httpClient.getConnectionManager().shutdown(); //Deprecated
         }
     }
 
-    public String MakePostRegistration(String email, String password,String nick) {
+    private HttpClient getHttpClient()
+    {
+        HttpClient httpClient = new DefaultHttpClient();
+        ((AbstractHttpClient) httpClient).setHttpRequestRetryHandler(new DefaultHttpRequestRetryHandler(0, false));
+        return httpClient;
+    }
+    private String createJsonString(Pair<String,Object> ... params) throws JSONException {
+        JSONObject toReturn = new JSONObject();
 
-        HttpClient httpClient = new DefaultHttpClient(); //Deprecated
-
-        try {
-            HttpPost request = new HttpPost("http://91.196.50.18:8088/api/users");
-
-            StringBuilder json = new StringBuilder();
-            json.append("{\"email\":\"");
-            json.append(email);
-            json.append("\",\"password\":\"");
-            json.append(password);
-            json.append("\",\"nick\":\"");
-            json.append(nick);
-            json.append("\"}");
-            System.out.println(json.toString());
-
-            StringEntity jsonParams =new StringEntity(json.toString());
-            request.addHeader("Content-Type", "application/json");
-            request.setEntity(jsonParams);
-            HttpResponse response = httpClient.execute(request);
-
-            // handle response here...
-            JSONObject message = GetJSON(PrintHttpResponse(response));
-            return message.getString("message");
-        }catch (Exception ex) {
-            return ex.toString();
-
-        } finally {
-            httpClient.getConnectionManager().shutdown(); //Deprecated
+        for(Pair<String,Object> pair : params)
+        {
+            toReturn.put(pair.first, pair.second);
         }
+        return toReturn.toString();
     }
 
-
-    public JSONObject GetJSON(String information) throws JSONException {
+    private JSONObject GetJSON(String information) throws JSONException {
         return new JSONObject(information);
     }
-    public String PrintHttpResponse(HttpResponse response) throws IOException {
+    private String PrintHttpResponse(HttpResponse response) throws IOException {
         BufferedReader r = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
         StringBuilder total = new StringBuilder();
