e99989980e3f4caace5a5dd100886e951e12ba3d
mikolajswiatek <mikolajswiatek@outlook.com>
2015-12-12 02:35:01 +0100

    Added GetAll button in Timesheet

--- client/app/TimeSheet/TimeSheetController.js
+++ client/app/TimeSheet/TimeSheetController.js
@@ -1,34 +1,53 @@
 'use strict';
 
 angular.module('dotCinemaApp')
-  .controller('TimeSheetController', function($scope, Timesheet, Auth) {
+  .controller('TimeSheetController', function($scope, Auth, Timesheet) {
       var User = Auth.getCurrentUser();
-      var Employer = User.employee_data;
 
       $scope.timesheets = new Array();
-      $scope.timesheet = $scope.timesheets.length > 0 ? 
-        $scope.timesheets[$scope.timesheets.length - 1] : {
-            _id: "",
-            date_start: "",
-            date_end: "",
-            employee_data_id: ""
-        };
 
       $scope.errors = [];
+
+      var Employer = User.employee_data;
         
       $scope.Init = function() {
         if (Employer != undefined && Employer._id != undefined) {
-          //Timesheet.GetAll()
           Timesheet.Gets(Employer)
           .success(function(timesheets) {
             $scope.timesheets = timesheets;
+            initTimesheet();
           })
           .error(function(error) {
             $scope.errors.push(error);
             console.log(error);
           });
         }
+        else {
+          initTimesheet();
+        }
       };
+
+      $scope.GetAll = function() {
+          Timesheet.GetAll()
+          .success(function(timesheets) {
+            $scope.timesheets = timesheets;
+            initTimesheet();
+          })
+          .error(function(error) {
+            $scope.errors.push(error);
+            console.log(error);
+          });
+      }
+
+      function initTimesheet() {
+        $scope.timesheet = $scope.timesheets.length > 0 ? 
+        $scope.timesheets[$scope.timesheets.length - 1] : {
+            _id: "",
+            date_start: "",
+            date_end: "",
+            employee_data_id: ""
+        };
+      }
 
       $scope.Start = function() {
         if ($scope.timesheet.date_end != "" || $scope.timesheets.length == 0) { 
